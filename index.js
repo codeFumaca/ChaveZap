8c47732dcb46d3dd524b116e241095413951b536
codeFumaça <fakecrazzyy@hotmail.com>
2023-12-19 00:35:23 -0300

    Add MongoDB integration and authentication strategy

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 import qrcode from 'qrcode-terminal';
 import messageHandler from './src/handlers/messagehandlers.js';
+import MongoStore from 'wwebjs-mongo/src/MongoStore.js';
+import mongoose from 'mongoose';
 import pkg from 'whatsapp-web.js';
-const { Client, LocalAuth } = pkg;
+import dotenv from 'dotenv'
 
-const client = new Client({ authStrategy: new LocalAuth() });
+const { Client, RemoteAuth } = pkg;
 
-client.on('qr', qr => {
-    qrcode.generate(qr, { small: true });
+dotenv.config()
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    const store = new MongoStore({ mongoose: mongoose });
+    const client = new Client({
+        authStrategy: new RemoteAuth({
+            store: store,
+            backupSyncIntervalMs: 300000
+        })
+    });
+
+    client.on('qr', qr => {
+        qrcode.generate(qr, { small: true });
+    });
+
+    client.on('ready', () => {
+        console.log('Client is ready!');
+    });
+
+    client.on('authenticated', session => {
+        console.log(session);
+    });
+
+    client.on('message', async msg => { messageHandler(msg) });
+
+    client.initialize();
 });
-
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
-
-client.on('message', async msg => { messageHandler(msg) });
-
-client.initialize();
