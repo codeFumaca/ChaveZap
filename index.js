5902d88b55eaca38da7f11e1e236d68154522717
codeFumaça <fakecrazzyy@hotmail.com>
2023-12-19 01:14:21 -0300

    Refactor code and update dependencies

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
 import qrcode from 'qrcode-terminal';
 import messageHandler from './src/handlers/messagehandlers.js';
-import MongoStore from 'wwebjs-mongo/src/MongoStore.js';
-import mongoose from 'mongoose';
+
 import pkg from 'whatsapp-web.js';
-import dotenv from 'dotenv'
 
-const { Client, RemoteAuth } = pkg;
+const { Client, LocalAuth } = pkg;
 
-dotenv.config()
-
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-    const store = new MongoStore({ mongoose: mongoose });
-    const client = new Client({
-        authStrategy: new RemoteAuth({
-            store: store,
-            backupSyncIntervalMs: 300000
-        })
-    });
-
-    client.on('qr', qr => {
-        qrcode.generate(qr, { small: true });
-    });
-
-    client.on('ready', () => {
-        console.log('Client is ready!');
-    });
-
-    client.on('message', async msg => { messageHandler(msg) });
-
-    client.initialize();
+const client = new Client({
+    authStrategy: new LocalAuth()
 });
+
+
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', async msg => { messageHandler(msg) });
+
+client.initialize();
