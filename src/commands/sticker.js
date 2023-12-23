92a1283e00ad8abe956b4f94ccd167ecfe85e8fc
codeFumaça <fakecrazzyy@hotmail.com>
2023-12-23 02:14:50 -0300

    Add FFMPEG_PATH environment variable

--- src/commands/sticker.js
+++ src/commands/sticker.js
@@ -1,6 +1,9 @@
+import Util from 'whatsapp-web.js/src/util/Util.js';
+
 export default async function stickerCommand(msg) {
     try {
         let msgAntiga;
+        Util.setFfmpegPath(process.env.FFMPEG_PATH);
 
         if (msg.hasQuotedMsg) {
             msgAntiga = await msg.getQuotedMessage();
@@ -8,12 +11,12 @@ export default async function stickerCommand(msg) {
             msgAntiga = msg;
         }
 
-        if (!msgAntiga.hasMedia) {
-            return msg.reply('Você precisa enviar uma imagem para que eu possa converter em sticker!');
-        }
+        if (!msgAntiga.hasMedia) { return msg.reply('Você precisa enviar uma imagem para que eu possa converter em sticker!'); }
+        
         await msg.react('🕣');
 
         const media = await msgAntiga.downloadMedia();
+
         await msg.reply(media, msg.from, {
             sendMediaAsSticker: true,
             stickerAuthor: `Solicitante: ${msg._data.notifyName}`,
