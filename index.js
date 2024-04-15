5f65003957a9faa5efc264ff1ecdb52b305537e3
codeFumaça <fakecrazzyy@hotmail.com>
2024-04-15 00:23:35 -0300

    Atualiza argumentos do puppeteer para desabilitar o setuid sandbox

--- index.js
+++ index.js
@@ -5,17 +5,12 @@ import pkg from 'whatsapp-web.js';
 
 const { Client, LocalAuth } = pkg;
 
-const wwebVersion = '2.2410.1';
 const client = new Client({
     restartOnAuthFail: true,
-    webVersionCache: {
-        remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/${wwebVersion}.html`,
-        type: 'remote',
-    },
     authStrategy: new LocalAuth(),
     puppeteer: {
         executablePath: process.env.CHROME_PATH,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        args: ['--no-sandbox', '--disable-setuid-sandbox',],
     }
 });
 
