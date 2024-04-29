7620e1c32954ad7c58a0be19f65cf54008efa3e1
codeFumaça <fakecrazzyy@hotmail.com>
2024-04-29 08:53:38 -0300

    alt: adicionado stories como opcao no comando instagram

--- src/commands/instagram.ts
+++ src/commands/instagram.ts
@@ -2,45 +2,64 @@ import axios from "axios";
 import { Message } from "whatsapp-web.js";
 
 import whatsappWeb from "whatsapp-web.js";
+import { MissingParameterError } from "../@types/Error";
 const MessageMedia = whatsappWeb.MessageMedia;
 
-export default async function instagramCommand(msg:Message) {
+export default async function instagramCommand(msg: Message) {
     try {
-        const linkInsta = msg.body.split(' ');
-
-        if (!linkInsta[1]) return msg.reply('Você precisa informar um link para que eu possa baixar!');
-
         await msg.react('🕣');
+        const op = msg.body.split(' ')[1];
+        const url = msg.body.split(' ')[2];
 
-        const linkDownload = await getResults(linkInsta[1]);
-
-        const media = await MessageMedia.fromUrl(linkDownload);
-
-        await msg.reply(media);
-
-        await msg.react('👍');
-
-    } catch (error:any) {
+        switch (op) {
+            case 'image':
+            case 'video':
+            case 'igtv':
+            case 'reels':
+                return await instagramMediaDownloader(msg, url, 'post');
+            case 'storie':
+                return await instagramMediaDownloader(msg, url, 'stories');
+            default:
+                await msg.react('❌');
+                return await msg.reply('Nenhuma opção encontrada.\nEscolha uma opção: <image | video | igtv | reels | storie>\nExemplo: ```f!instagram image https://www.instagram.com/p/...```');
+        }
+    } catch (error: any) {
         await msg.reply(`Ocorreu um erro: _*${error.message}*_`);
         await msg.react('❌');
     }
 }
 
-async function getResults(linkInsta:string) {
+async function instagramMediaDownloader(msg: Message, url: string, type: string) {
+    try {
+        if(!url) throw new MissingParameterError();
+        
+        const linkDownload = await getResults(url, type);
 
-    const options = {
-        method: 'GET',
-        url: 'https://instagram-media-downloader.p.rapidapi.com/rapid/post.php',
-        params: {
-            url: linkInsta
-        },
-        headers: {
-            'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
-            'X-RapidAPI-Host': 'instagram-media-downloader.p.rapidapi.com'
-        }
-    };
+        const media = await MessageMedia.fromUrl(linkDownload);
 
-    const response = await axios.request(options);
-    if (!response.data.video) return response.data.image;
-    return response.data.video;
+        await msg.reply(media);
+
+        return await msg.react('👍');
+    } catch (error: any) {
+        await msg.reply(`Ocorreu um erro: _*${error.message}*_`);
+        await msg.react('❌');
+    }
+
+    async function getResults(linkPost: string, type: string) {
+        const options = {
+            method: 'GET',
+            url: `https://instagram-media-downloader.p.rapidapi.com/rapid/${type}.php`,
+            params: {
+                url: linkPost
+            },
+            headers: {
+                'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
+                'X-RapidAPI-Host': 'instagram-media-downloader.p.rapidapi.com'
+            }
+        };
+
+        const response = await axios.request(options);
+        if (!response.data.video) return response.data.image;
+        return response.data.video;
+    }
 }
