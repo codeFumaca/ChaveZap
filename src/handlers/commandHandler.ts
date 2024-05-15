7665296136b444ab266c4fb943586d343ff0e1d8
codeFumaça <fakecrazzyy@hotmail.com>
2024-05-15 11:58:55 -0300

    feat: Update commandHandler to use new command file structure

--- src/handlers/commandHandler.ts
+++ src/handlers/commandHandler.ts
@@ -1,17 +1,18 @@
-import pingCommand from '../commands/ping.ts';
-import stickerCommand from '../commands/sticker.ts';
-import imageCommand from '../commands/image.ts';
-import bichoCommand from '../commands/bicho.ts';
-import rastreioCommand from '../commands/rastreio.ts';
-import instagramCommand from '../commands/instagram.ts';
-import gptCommand from '../commands/gptChat.ts';
-import versiculoCommand from '../commands/versiculo.ts';
-import log from './log.ts';
-import { everyoneCommand, playCommand, aramCommand } from '../commands/mentions.ts';
+import pingCommand from '../commands/fun/ping.ts';
+import stickerCommand from '../commands/fun/sticker.ts';
+import imageCommand from '../commands/fun/image.ts';
+import bichoCommand from '../commands/fun/bicho.ts';
+import rastreioCommand from '../commands/fun/rastreio.ts';
+import instagramCommand from '../commands/fun/instagram.ts';
+import gptCommand from '../commands/fun/gptChat.ts';
+import versiculoCommand from '../commands/fun/versiculo.ts';
+import log, { logError } from './log.ts';
+import { everyoneCommand, playCommand, aramCommand } from '../commands/fun/mentions.ts';
 import { Client, Message } from 'whatsapp-web.js';
 import withLogging from './log.ts';
+import scheduleCommand from '../commands/utility/schedule.ts';
 
-const commands:any = {
+const commands: any = {
     'f!gpt': gptCommand,
     'f!i': imageCommand,
     'f!ping': pingCommand,
@@ -24,17 +25,26 @@ const commands:any = {
     'f!rastreio': rastreioCommand,
     'f!instagram': instagramCommand,
     'f!versiculo': versiculoCommand,
+    'f!escala': scheduleCommand,
 };
 
-export default async function commandHandler(message:Message, client:Client) {
+export default async function commandHandler(message: Message, client: Client) {
+
+    message.body = message.body.toLocaleLowerCase();
 
     if (!message.body.startsWith('f!')) return;
 
-    const command:string = message.body.split(' ')[0];
+    const command: string = message.body.split(' ')[0];
 
     if (commands[command]) {
-        const commandWithLogging = withLogging(commands[command]);
-        await commandWithLogging(message, client);
+        try {
+            const commandWithLogging = withLogging(commands[command]);
+            await commandWithLogging(message, client);
+        } catch (error) {
+            if (error instanceof Error) logError(error, client);
+            message.react('❌');
+            message.reply('Algo deu errado, tente novamente.');
+        }
     } else {
         message.react('❓');
         message.reply(`Comando desconhecido: ${command}`);
--- src/handlers/log.ts
+++ src/handlers/log.ts
@@ -2,13 +2,13 @@ import { Client, Message } from "whatsapp-web.js";
 import { RecievedMessage } from "../@types/RecievedMessages.ts";
 
 export default function withLogging(commandFunc: (msg: Message, client: Client) => Promise<void>) {
-    return async function(msg: Message, client: Client) {
+    return async function (msg: Message, client: Client) {
         const groupId = process.env.LOGCHANNEL_ID;
 
-        if (!groupId) return console.log('Log channel not found');
+        if (!groupId) return msg.reply('Log channel not found, contact the developer.');
 
         const groupChat = await client.getChatById(groupId);
-        
+
         let recievedMessage = msg as unknown as RecievedMessage;
         let text = `📢 *Novo comando executado!*\nSolicitante: ${recievedMessage._data.notifyName}\nComando: ${commandFunc.name}`;
 
@@ -16,4 +16,18 @@ export default function withLogging(commandFunc: (msg: Message, client: Client) 
 
         return commandFunc(msg, client);
     }
+}
+
+export function logError(error: Error, client: Client) {
+    const groupId = process.env.LOGCHANNEL_ID;
+
+    if (!groupId) return;
+
+    const groupChat = client.getChatById(groupId);
+
+    let text = `❌ *Erro!*\n${error.message}`;
+
+    groupChat.then((chat) => {
+        chat.sendMessage(text);
+    });
 }
