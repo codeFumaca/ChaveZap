8bfe11ed2403d723de44902bd3843753be442b9f
codeFumaça <fakecrazzyy@hotmail.com>
2024-07-05 00:01:01 -0300

    update commandHandler to handle media commands

--- src/handlers/commandHandler.ts
+++ src/handlers/commandHandler.ts
@@ -3,7 +3,7 @@ import stickerCommand from '../commands/fun/sticker.ts';
 import imageCommand from '../commands/fun/image.ts';
 import bichoCommand from '../commands/fun/bicho.ts';
 import rastreioCommand from '../commands/fun/rastreio.ts';
-import instagramCommand from '../commands/fun/instagram.ts';
+import mediaCommand from '../commands/fun/media.ts';
 import gptCommand from '../commands/fun/gptChat.ts';
 import versiculoCommand from '../commands/fun/versiculo.ts';
 import log, { logError } from './log.ts';
@@ -12,41 +12,44 @@ import { Client, Message } from 'whatsapp-web.js';
 import withLogging from './log.ts';
 import scheduleCommand from '../commands/utility/schedule.ts';
 
+const prefix = process.env.PREFIX;
+
 const commands: any = {
-    'f!gpt': gptCommand,
-    'f!i': imageCommand,
-    'f!ping': pingCommand,
-    'f!s': stickerCommand,
-    'f!aram': aramCommand,
-    'f!play': playCommand,
-    'f!bicho': bichoCommand,
-    'f!paz': versiculoCommand,
-    'f!everyone': everyoneCommand,
-    'f!rastreio': rastreioCommand,
-    'f!instagram': instagramCommand,
-    'f!versiculo': versiculoCommand,
-    'f!escala': scheduleCommand,
+    'gpt': gptCommand,
+    'i': imageCommand,
+    'ping': pingCommand,
+    's': stickerCommand,
+    'aram': aramCommand,
+    'play': playCommand,
+    'bicho': bichoCommand,
+    'paz': versiculoCommand,
+    'everyone': everyoneCommand,
+    'rastreio': rastreioCommand,
+    'media': mediaCommand,
+    'versiculo': versiculoCommand,
+    'escala': scheduleCommand,
 };
 
 export default async function commandHandler(message: Message, client: Client) {
+    try {
+        if (!prefix) throw new Error('Prefixo não definido.');
 
-    message.body = message.body.toLocaleLowerCase();
+        if (!message.body.startsWith(prefix)) return;
+        if (message.body.startsWith(prefix)) message.body = message.body.replace(prefix, '');
+        const command: string = message.body.split(' ')[0];
 
-    if (!message.body.startsWith('f!')) return;
+        if (commands[command]) {
 
-    const command: string = message.body.split(' ')[0];
-
-    if (commands[command]) {
-        try {
             const commandWithLogging = withLogging(commands[command]);
             await commandWithLogging(message, client);
-        } catch (error) {
-            if (error instanceof Error) logError(error, client);
-            message.react('❌');
-            message.reply('Algo deu errado, tente novamente.');
+
+        } else {
+            message.react('❓');
+            message.reply(`Comando desconhecido: ${command}`);
         }
-    } else {
-        message.react('❓');
-        message.reply(`Comando desconhecido: ${command}`);
+    } catch (error) {
+        if (error instanceof Error) logError(error, client);
+        message.react('❌');
+        message.reply('Algo deu errado, tente novamente.');
     }
 };
